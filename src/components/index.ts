ffb700289980eaeceae1c329fe9a8362e82760f1
Leonardo Santos <leosantosmelo07@gmail.com>
2026-03-28 22:14:24 -0300

    refactor: components

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,7 +2,6 @@ export * from "./addBirdFormActions";
 export * from "./birdCard";
 export * from "./birdEmpty";
 export * from "./habitatsCard";
-export * from "./habitatsOptions";
 export * from "./header";
 export * from "./loadingSpinner";
 export * from "./pagination";
